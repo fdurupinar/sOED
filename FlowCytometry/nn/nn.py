046634431e2ae2fedccdc1e4fc041a1a797531e9
fdurupinar <f.durupinar@gmail.com>
2018-04-27 16:12:20 -0700

    started nn

--- FlowCytometry/nn/nn.py
+++ FlowCytometry/nn/nn.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
 import numpy as np
+from ga.scoreHandler import ScoreHandler
 from util.staticMethods import StaticMethods
 import operator
 
@@ -15,7 +16,7 @@ C_CNT = 5  # cancer patients
 
 CELL_CNT = 100
 
-
+POPULATION_SIZE = ANTIBODY_CNT / 4  # 40  # make this divisible by 4
 MARKERS_LIST = [[0, 5, 10, 15]]
 MU_LIST = [0.99]
 STD_DEV_LIST = [0.01]
@@ -24,22 +25,46 @@ MUTATION_PROBABILITY = 0.1  # 0.03
 CROSS_OVER_2_RATIO = 0.5
 VISUALIZE_POPULATION = False
 
-
+INPUT_DIM = 4
 class NNSolver:
 
-    def __init__(self, antibody_cnt, nc_cnt, c_cnt, markers_list,  cell_cnt,
+    def __init__(self, population_size,antibody_cnt, nc_cnt, c_cnt, markers_list,  cell_cnt,
                  mu_list, sigma_list):
 
+        self.population_size = population_size
+        self.antibody_cnt = antibody_cnt
+        self.nc_cnt = nc_cnt
+        self.c_cnt = c_cnt
+
+        self.init_data()
+
+        self.run_simulation()
 
         return
 
+    def init_data(self):
+
+        self.population = np.full((self.population_size, 4), 0, dtype=np.int)
+        self.labels = np.full((self.population_size, 4), 0, dtype=np.int)
+        i = 0
+        while i < ANTIBODY_CNT:
+            ab_arr = np.array([i, i + 1, i + 2, i + 3])
+            ab_arr = np.sort(ab_arr)
+            self.population[i / 4] = self.score_handler.compute_max_precision_for_ab_combination(ab_arr)
+            self.labels[i / 4] = ab_arr
+
+            i += 4
+
     def run_simulation(self):
         # Loading the data set (PIMA Diabetes Dataset)
-        dataset = np.loadtxt('datasets/pima-indians-diabetes.csv', delimiter=",")
+        # dataset = np.loadtxt('datasets/pima-indians-diabetes.csv', delimiter=",")
+        #
+        # # Loading the input values to X and Label values Y using slicing.
+        # X = dataset[:, 0:4]
+        # Y = dataset[:, 4]
 
-        # Loading the input values to X and Label values Y using slicing.
-        X = dataset[:, 0:4]
-        Y = dataset[:, 4]
+        X = self.population[:, 0:4]
+        Y = self.labels[:, 0:4]
 
         # Initializing the Sequential model from KERAS.
         model = Sequential()
